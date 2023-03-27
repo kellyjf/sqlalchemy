218ed12dee5e715192b8918a6a41d97462025a86
John Kelly <kellyjf@yahoo.com>
2023-03-27 16:29:05 -0600

    check

--- database.py
+++ database.py
@@ -15,6 +15,7 @@ Base=declarative_base()
 # and constraints of the table.  These are instances of classes that are available 
 # in the main sqlalchemy module
 from sqlalchemy import Index, Column, Boolean, Integer, String, Unicode, DateTime, ForeignKey, func
+import string
 
 class Verb(Base):
     __tablename__ = "verbs"
@@ -23,7 +24,6 @@ class Verb(Base):
     past_part = Column(String)
     gerund = Column(String)
     def __init__(self, **kw):
-      print(kw)
       if not kw.get('past_part'):
         self.past_part = kw.get('id')[0:-1]+"do"
         if self.past_part[-2:-2]=='e':
@@ -34,8 +34,22 @@ class Verb(Base):
 
   
     def conjugate(self, tense, subject):
+        match=[x.text for x in self.conjugations if x.tense==tense and x.person==subject.person]
+        if not match:
+            if not tense.compound:
+                defcon=self.id[-2:]
+                tverb=Verb._template.get(defcon)
+                match=[x.text for x in tverb.conjugations if x.tense==tense and x.person==subject.person][0]
+                match=match.replace("STEM",self.id[0:-2])         
+                match=match.replace("INF",self.id)         
+            else:
+                match=[x.text for x in tense.aux.conjugations if x.tense==tense.aux_tense and x.person==subject.person][0]
+                if tense.aux_id=="estar":
+                    match=match+" "+self.gerund
+                else:
+                    match=match+" "+self.past_part
+        return match
 
-     return f"{Verb._ter!r} {self.past_part}"
 
     def __repr__(self):
         return f"Verb({self.id!r}={self.regular!r}, {self.past_part!r}, {self.gerund!r})" 
@@ -45,7 +59,8 @@ class Tense(Base):
     id = Column(String, primary_key=True)
     name = Column(String)
     compound = Column(Boolean)
-    aux_id = Column(String, ForeignKey("verbs.id"))
+    aux_id = Column(String, ForeignKey("verbs.id"), nullable=True)
+    aux_tense_id = Column(String, ForeignKey("tenses.id"), nullable=True)
 
 
     def __repr__(self):
@@ -74,8 +89,10 @@ class Conjugation(Base):
 
 from sqlalchemy.orm import relationship
 Conjugation.verb = relationship("Verb", back_populates="conjugations")
+Conjugation.tense = relationship("Tense")
 Verb.conjugations = relationship("Conjugation", back_populates="verb")
 Tense.aux = relationship("Verb")
+Tense.aux_tense = relationship("Tense",remote_side=Tense.id)
 
 
 class Clause(Base):
@@ -165,7 +182,8 @@ def load():
     session.add(Conjugation(person="part", tense_id="ip", verb_id="estar", text="estando"))
     session.add(Conjugation(person="part", tense_id="ipp", verb_id="estar", text="estado"))
 
-    session.add(Tense(id="ippr", name="Indicative Présent Progressive", aux_id="estar"))
+    session.add(Tense(id="ippr", name="Indicative Présent Progressive", 
+            compound=True, aux=estar, aux_tense_id='ip'))
 
 
     ter=Verb(id="ter", regular=False)
@@ -185,12 +203,65 @@ def load():
     session.add(Conjugation(person="1pp", tense_id="ipp", verb_id="ter", text="tivemos"))
     session.add(Conjugation(person="3pp", tense_id="ipp", verb_id="ter", text="tiveram"))
 
-    session.add(Conjugation(person="part", tense_id="ip", verb_id="ter", text="tendo"))
-    session.add(Conjugation(person="part", tense_id="ipp", verb_id="ter", text="tido"))
-    session.add(Tense(id="itpr", name="Indicative Préterit Progressive", aux_id="ter"))
+    session.add(Tense(id="itpr", name="Indicative Préterit Progressive", 
+            compound=True, aux=ter, aux_tense_id='ip'))
+
+
+    dar=Verb(id="*ar", regular=True)
+    session.add(dar)
+    session.add(Conjugation(person="1ps", tense_id="ip", verb_id="*ar", text="STEMo"))
+    session.add(Conjugation(person="3ps", tense_id="ip", verb_id="*ar", text="STEMa"))
+    session.add(Conjugation(person="1pp", tense_id="ip", verb_id="*ar", text="STEMamos"))
+    session.add(Conjugation(person="3pp", tense_id="ip", verb_id="*ar", text="STEMam"))
+
+    session.add(Conjugation(person="1ps", tense_id="ipi", verb_id="*ar", text="STEMava"))
+    session.add(Conjugation(person="3ps", tense_id="ipi", verb_id="*ar", text="STEMava"))
+    session.add(Conjugation(person="1pp", tense_id="ipi", verb_id="*ar", text="STEMavamos"))
+    session.add(Conjugation(person="3pp", tense_id="ipi", verb_id="*ar", text="STEMavam"))
+
+    session.add(Conjugation(person="1ps", tense_id="ipp", verb_id="*ar", text="STEMei"))
+    session.add(Conjugation(person="3ps", tense_id="ipp", verb_id="*ar", text="STEMou"))
+    session.add(Conjugation(person="1pp", tense_id="ipp", verb_id="*ar", text="STEMamos"))
+    session.add(Conjugation(person="3pp", tense_id="ipp", verb_id="*ar", text="STEMaram"))
+
+
+    dar=Verb(id="*er", regular=True)
+    session.add(dar)
+    session.add(Conjugation(person="1ps", tense_id="ip", verb_id="*er", text="STEMo"))
+    session.add(Conjugation(person="3ps", tense_id="ip", verb_id="*er", text="STEMe"))
+    session.add(Conjugation(person="1pp", tense_id="ip", verb_id="*er", text="STEMemos"))
+    session.add(Conjugation(person="3pp", tense_id="ip", verb_id="*er", text="STEMem"))
+
+    session.add(Conjugation(person="1ps", tense_id="ipi", verb_id="*er", text="STEMia"))
+    session.add(Conjugation(person="3ps", tense_id="ipi", verb_id="*er", text="STEMia"))
+    session.add(Conjugation(person="1pp", tense_id="ipi", verb_id="*er", text="STEMiamos"))
+    session.add(Conjugation(person="3pp", tense_id="ipi", verb_id="*er", text="STEMiam"))
+
+    session.add(Conjugation(person="1ps", tense_id="ipp", verb_id="*er", text="STEMi"))
+    session.add(Conjugation(person="3ps", tense_id="ipp", verb_id="*er", text="STEMeu"))
+    session.add(Conjugation(person="1pp", tense_id="ipp", verb_id="*er", text="STEMemos"))
+    session.add(Conjugation(person="3pp", tense_id="ipp", verb_id="*er", text="STEMeram"))
+
+
+    dar=Verb(id="*ir", regular=True)
+    session.add(dar)
+    session.add(Conjugation(person="1ps", tense_id="ip", verb_id="*ir", text="STEMo"))
+    session.add(Conjugation(person="3ps", tense_id="ip", verb_id="*ir", text="STEMe"))
+    session.add(Conjugation(person="1pp", tense_id="ip", verb_id="*ir", text="STEMimos"))
+    session.add(Conjugation(person="3pp", tense_id="ip", verb_id="*ir", text="STEMem"))
+
+    session.add(Conjugation(person="1ps", tense_id="ipi", verb_id="*ir", text="STEMia"))
+    session.add(Conjugation(person="3ps", tense_id="ipi", verb_id="*ir", text="STEMia"))
+    session.add(Conjugation(person="1pp", tense_id="ipi", verb_id="*ir", text="STEMiamos"))
+    session.add(Conjugation(person="3pp", tense_id="ipi", verb_id="*ir", text="STEMiam"))
+
+    session.add(Conjugation(person="1ps", tense_id="ipp", verb_id="*ir", text="STEMi"))
+    session.add(Conjugation(person="3ps", tense_id="ipp", verb_id="*ir", text="STEMiu"))
+    session.add(Conjugation(person="1pp", tense_id="ipp", verb_id="*ir", text="STEMimos"))
+    session.add(Conjugation(person="3pp", tense_id="ipp", verb_id="*ir", text="STEMiram"))
+
+
     session.commit()
-
-
 
 
 # a=session.query(Conjugation).filter(Conjugation.verb_id.in_(["estar","vir"])).filter(Conjugation.tense_id=="ipi")
@@ -206,9 +277,20 @@ session=Session(engine)
 def finish(session):
     Verb._estar = session.query(Verb).filter(Verb.id=="estar").first()
     Verb._ter = session.query(Verb).filter(Verb.id=="ter").first()
-finish()
+    Verb._template = {
+          'ar': session.query(Verb).filter(Verb.id=="*ar").first(),
+          'er': session.query(Verb).filter(Verb.id=="*er").first(),
+          'ir': session.query(Verb).filter(Verb.id=="*ir").first()}
+
+#finish(session)
 
 
+create()
+load()
+finish(session)
+v=Verb(id='falar')
+s=session.query(Subject).all()
+t=session.query(Tense).all()
 
 
 from argparse import ArgumentParser as ap
