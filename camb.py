639e37a1901cea15ad53a81c375f55a3e815d72e
John Kelly <kellyjf@yahoo.com>
2023-04-08 09:31:34 -0600

    check

--- camb.py
+++ camb.py
@@ -1,32 +1,49 @@
 #!/usr/bin/python3
 
 
-import requests
-from lxml import html
-import sqlite3
-from schema import Synonym,Definition,Category,Statistic, Subject, Verb, Tense, Conjugation, Sentence, Rule, State, Base, init
 import os.path
 import re
 import subprocess 
 
-from sqlalchemy import create_engine, not_
-engine=create_engine("sqlite:///portuguese.sqlite")
-Base.metadata.create_all(engine)
 
-from sqlalchemy.orm import Session
-session=Session(engine)
+#
+# SQLAlchemy creates an object heirarchy that adapts python code classes
+# to DB tables.  By subclassing all these adapter objects from a declariative
+# base table, code can traverse the object tree to detect the structure of 
+# the object heirarchy, and create schema DDL so that the database can be created
+# from the derived SQL
+#
+from sqlalchemy.ext.declarative import declarative_base
+Base=declarative_base()
+
+#
+# The classes represent tables, and they contain members for the columns
+# and constraints of the table.  These are instances of classes that are available 
+# in the main sqlalchemy module
+from sqlalchemy import Index, Column, Boolean, Integer, String, Unicode, DateTime, ForeignKey, func
+import string
 
 
-# verb - lookup key onweb and file name
-# infinitivo -infinitive form of the verb, for text output and database keys	
-def load_ipa(verb):
-	verbreg=True
-	cmd = ['espeak', '-q', '-v', 'pt-BR', '--ipa', verb.id ]
+
+class Word(Base):
+	__tablename__ = "words"
+
+	id = Column(String, primary_key=True)
+	gendered = Column(String)
+	pron = Column(String)
+	define = Column(String)
+	example = Column(String)
+	picture = Column(String)
+	ipa = Column(String)
+
+
+# word - lookup key onweb and file name
+# infinitivo -infinitive form of the word, for text output and database keys	
+def load_ipa(word):
+	cmd = ['espeak', '-q', '-v', 'pt-BR', '--ipa', word.id ]
 	p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 	p.wait()
-	verb.ipa=p.stdout.read().strip().decode('utf-8')
-	session.add(verb)
-	session.commit()			
+	word.ipa=p.stdout.read().strip().decode('utf-8')
 
 
 from argparse import ArgumentParser as ap
@@ -34,12 +51,20 @@ if __name__ == "__main__":
 	parser=ap()
 	parser.add_argument("--rules-only","-r", action="store_true", help="Only load sentence templates")
 	args=parser.parse_args()
-	for cat in session.query(Category).all():
-		session.delete(cat)
+
+	from sqlalchemy import create_engine, not_
+	from sqlalchemy.orm import Session
+
+	engine=create_engine("sqlite:///vocab.sqlite")
+	Base.metadata.create_all(engine)
+	session=Session(engine)
+
+	with open("vocab.txt","r") as f:
+		for line in f.readlines()[16:]:
+			[vid,gendered,pron,define,example,picture]=line.strip().split("\t")[0:6]
+			word=Word(id=vid,gendered=gendered,pron=pron,define=define,example=example,picture=picture)
+			load_ipa(word)
+			session.add(word)
 	session.commit()
-	#for verb in session.query(Verb).filter(not_(Verb.id.like('*%'))).all():
-	for verb in session.query(Verb).all():
-			print(f"IPA for {verb.id}")
-			load_ipa(verb)
 
 
